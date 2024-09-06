fda4398191812b284bf0ca78388b9aa13bbbaf81
Oscar Roberts <roberts_oscar@hotmail.com>
2024-09-06 14:17:46 -0300

    contador de palabras

---
+++ contador-letras.py
@@ -0,0 +1,7 @@
+# 1. solicitar que se ingrese una palabra
+palabra_ingresada = input('Por favor, ingresa una palabra: ')
+# 2. contar la cantidad de letras en la palabra ingresada
+cantidad_letras = len(palabra_ingresada)
+# 3. mostrar al usuario el resultado del conteo
+print(f'La palabra ingresada tiene {cantidad_letras} letras')
+
